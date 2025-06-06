26a3dda5078fa5472a981cad72ad1dfdbf90f9af
Mateus <ext.mateus@icloud.com>
2025-06-06 16:58:37 -0300

    Add: Adicionado novamente spring-security, e desligando os filtros para o MockMvc
    Os testes não estavam sendo autenticados corretamente.
    Isso acontece porque o contexto de teste do @WebMvcTest inicializa apenas os
    beans relacionados ao MVC e ignora outras configurações, e isso inclui
    meu SecurityConfig.

--- votos/src/test/java/mateus/votos/controller/PautaControllerTest.java
+++ votos/src/test/java/mateus/votos/controller/PautaControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -20,6 +21,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(PautaController.class)
 class PautaControllerTest {
 
